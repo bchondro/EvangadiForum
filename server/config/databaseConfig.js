8faa1ddaa510b2fe815fb813fd49498353d91ffd
bchondro <berhanuchondro81@gmail.com>
2025-06-27 14:03:21 -0400

    add evangadi forum

---
+++ server/config/databaseConfig.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import mysql from "mysql2/promise";
+
+dotenv.config();
+const dbconfig = {
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || '4000',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME || 'evangadi_networks',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+
+}
+
+// Creating connection pool
+const pool = mysql.createPool(dbconfig);
+
+export default pool;
