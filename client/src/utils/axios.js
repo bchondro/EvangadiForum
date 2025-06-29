214cde9ba142053da17983b0594b0156c4e539f0
bchondro <berhanuchondro81@gmail.com>
2025-06-29 15:00:09 -0400

    fix

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  //  baseURL: "http://localhost:3000/api",
   baseURL: "https://evangadiforum-1-m1vk.onrender.com/api"
-
 });
 
 export default axiosInstance;
