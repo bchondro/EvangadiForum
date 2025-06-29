cdd0cd5050bc290d23afdf87df9cec9b9162d262
bchondro <berhanuchondro81@gmail.com>
2025-06-29 17:50:01 -0400

    fix

--- server/config/databaseConfig.js
+++ server/config/databaseConfig.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 
 dotenv.config();
 const dbconfig = {
-  host: process.env.DB_HOST ,
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
